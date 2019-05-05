8678628f744fe92c7aaab9cebce44d643e1ef8b1
Andrei Kuzniatsou <andrei_kuzniatsou@epam.com>
2019-05-05 19:44:55 +0300

    init database structure

---
+++ sql/create_table.sql
@@ -0,0 +1,44 @@
+CREATE SEQUENCE IF NOT EXISTS public.owner_seq START 107;
+CREATE SEQUENCE IF NOT EXISTS public.vehicle_seq START 205;
+
+
+CREATE TABLE IF NOT EXISTS public.owner
+(
+  id bigint NOT NULL DEFAULT nextval('owner_seq'::regclass),
+  name text NOT NULL,
+  pass_id text NOT NULL,
+  last_name text,
+  address text,
+  phone text,
+  CONSTRAINT pk_onwer_id PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.vehicle
+(
+  id bigint NOT NULL DEFAULT nextval('vehicle_seq'::regclass),
+  owner_id bigint NOT NULL,
+  type text,
+  "number" text NOT NULL,
+  body_number text NOT NULL,
+  model text,
+  CONSTRAINT pk_vehicle_id PRIMARY KEY (id),
+  CONSTRAINT fk_owner_id_vehicle_id FOREIGN KEY (owner_id)
+      REFERENCES public.owner (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+
+CREATE TABLE IF NOT EXISTS public.damage
+(
+  id bigint NOT NULL,
+  vehicle_id bigint NOT NULL,
+  date timestamp with time zone NOT NULL,
+  zone1 boolean,
+  zone2 boolean,
+  zone3 boolean,
+  CONSTRAINT damage_pk PRIMARY KEY (id),
+  CONSTRAINT fk_damage_vehicle_id FOREIGN KEY (vehicle_id)
+      REFERENCES public.vehicle (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
---
+++ sql/init_table.sql
@@ -0,0 +1,19 @@
+INSERT INTO public.owner (id, name, pass_id, last_name, address, phone) VALUES
+    (101, 'Andrei', '5d7825cc-2fdf-4f42-9be2-59f1c1525291', 'Provan', 'Vitebsk', '+375-33-100-14-14'),
+    (102, 'Mikhail', '621e67a9-8214-465e-8838-196869893687', 'Zadov', 'Vitebsk', '+375-33-100-14-90'),
+    (103, 'Dzmitry', 'd4f4d6d1-edc2-459d-a547-aa87195447a1', 'Klimchanka', 'California', '+375-33-100-14-89'),
+    (104, 'Viktar', '3da7685e-91f7-4540-9cab-1d9c18285f1e', 'Lukarev', 'Volozhin', '+375-33-100-14-23'),
+    (105, 'Alena', 'e5a60db8-2be2-40fe-9798-a5215645d51b', 'Zaviraiko', 'Vitebsk', '+375-33-100-14-45'),
+    (106, 'Valentin', '876e67af-0920-4e0d-a9a7-7e4352251450', 'Pruprunkevich', 'Vitebsk', '+375-33-100-14-67');
+
+INSERT INTO public.vehicle (id, owner_id, type, number, body_number, model) VALUES
+    (201, 101, 'VAN', '00-01 DF', '91c5896b-1b5f-4669-a88f-790b01148c2e', 'OPEL ZAFIRA'),
+    (202, 102, 'MOTO', '04-56 HG', '1706d480-3bf9-44c8-b3a2-3e6a2bd2b1b3', 'UAZ PATRIOT'),
+    (203, 103, 'BUS', '02-03 FG', '2a25cdf1-c38a-4130-9485-84ac6c49f00c', 'CITROEN XSARA PICASSO'),
+    (204, 104, 'JEEP', '90-34 FG', 'eade86be-f0d0-4a49-8826-3939a5465317', 'TOYOTA RAW 4');
+
+INSERT INTO public.damage (id, vehicle_id, date, zone1, zone2, zone3) VALUES
+    (301, 201, '2019-04-27 19:19:21.630000', true, false, false),
+    (302, 203, '2019-04-27 19:19:21.630000', true, false, true),
+    (303, 203, '2019-01-27 19:19:21.630000', true, false, true),
+    (304, 202, '2019-04-29 19:19:21.630000', true, false, true);
